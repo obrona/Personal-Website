78c4546a9faeb1cd830d1f8f8c978e1c4c46eba4
obrona <tlim8772@gmail.com>
2026-05-25 13:39:50 +0800

    commit

--- src/Main/Bio.tsx
+++ src/Main/Bio.tsx
@@ -5,7 +5,7 @@ export function Bio() {
     <div className="h-full w-full flex flex-col items-center">
       <div className="flex gap-4 items-center justify-center mt-32">
         <Saturn height='180px' width='180px' />
-        <div>
+        <div className='space-y-1'>
           <div>Name: Tim</div>
           <div>Nationality: Singapore Citizen</div>
           <div>Email: tlim8772@gmail.com</div>
